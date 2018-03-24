4d691498944a0a5e2168acc14cbaf794767e5fbc
Daniel Bastos <daniel@viperit.com.br>
2018-03-24 20:49:45 -0300

    Commit Inicial

---
+++ index.js
@@ -0,0 +1,23 @@
+var express =  require ('express');
+var consign = require('consign');
+
+const app = express();
+
+//app.get("/", (req,res) => res.json({status: "Essa é a Minha API"}));
+
+/*app.get("/users", (req,res) => {
+  res.json({
+    users: [
+      {nome: "Daniel"},
+      {nome: "Fabiola"}
+    ]
+    });
+  });
+*/
+
+consign()
+  .include("models")
+  .then("libs/middlewares.js")
+  .then("routes")
+  .then("libs/boot.js")
+  .into(app);
